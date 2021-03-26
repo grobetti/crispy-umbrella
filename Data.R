01df4bb05d4f2c94d4c683f64aae1428765afbf2
grobetti <80690502+grobetti@users.noreply.github.com>
2021-03-26 14:22:31 +0100

    Update Data.R

--- Data.R
+++ Data.R
@@ -48,6 +48,19 @@ summary(lm)
 plm <- plm(data$tscorek ~ data$sck, model = "within", index = c("schidkn"), data = data)
 summary(plm)
 
+#AG#
+fit1 <- lm(tscorek ~ sck, data = data) #fit linear regression
+par(mfrow = c(2,2))
+plot(fit1, which = 2:5) #diagnostic plots
+par(mfrow = c(1,1))
+summary(fit1)
+fit2 <- plm(tscorek ~ sck, index = c("schidkn"), model = "within", data = data) #fit linear regression with school fixed effects
+summary(fit2)
+fitcheck <- lmer(tscorek ~ 1 + sck + (1|schidkn), data = data) #same with another package
+summary(fitcheck)
+sqrt(var(data$tscorek)) #get standard deviation
+#AG#
+
 ```
 
 3) Standard Errors
